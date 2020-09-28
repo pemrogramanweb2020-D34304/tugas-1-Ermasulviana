543188d526b8b016fee7190ed609899bc1b26e5e
Ermasulviana <71263691+Ermasulviana@users.noreply.github.com>
2020-09-28 23:22:36 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,1 +1,22 @@
+<?php 
+	if(isset($_GET['hitung'])){
+		$bil1 = $_GET['bilangan1'];
+		$bil2 = $_GET['bilangan2'];
+		$operasi = $_GET['operasi'];
+		switch ($operasi) {
+			case 'tambah':
+				$hasil = $bilangan1+$bilangan2;
+			break;
+			case 'kurang':
+				$hasil = $bilangan1-$bilangan2;
+			break;
+			case 'kali':
+				$hasil = $bilangan1*$bilangan2;
+			break;
+			case 'bagi':
+				$hasil = $bilangan1/$bilangan2;
+			break;			
+		}
+	}
+	?>
 
